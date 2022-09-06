13dae0fc532a868ce26f595bee03aacac8a25aa5
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2022-09-06 18:36:34 +0700

    Add files via upload

---
+++ sfupload.sh
@@ -0,0 +1,21 @@
+cd SGSIs
+ls
+expect -c "
+spawn sftp zualolicongsis@frs.sourceforge.net
+expect \"yes/no\"
+send \"yes\r\"
+expect \"Password\"        
+send \"sc12212211\r\"
+expect \"sftp> \"
+send \"cd GSI\r\"
+set timeout -1
+send \"mkdir oniiupl\r\"
+expect \"sftp> \"
+send \"cd oniiupl\r\"
+set timeout -1
+send \"put *.zip\r\"
+expect \"Uploading\"
+expect \"100%\"
+expect \"sftp>\"
+send \"bye\r\"
+interact"
